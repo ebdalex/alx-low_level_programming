70ad5171ba936ce23fe770bd8911f3f52ff1bd8d
ebdalex <effahboadialex@gmail.com>
2023-03-24 04:43:37 -0700

    Moree functions, more nested

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -7,12 +7,9 @@
  */
 int main(void)
 {
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
     return (0);
 }
